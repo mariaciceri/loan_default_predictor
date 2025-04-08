a9b8bd38dc8f71ee13c3be58f46f5f8274e9fbb8
Maria <mariafacchin00@hotmail.com>
2025-04-08 08:58:20 +0200

    Create streamlit multipage app

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -1,5 +1,33 @@
 import streamlit as st 
 
 
+class MultiPage:
+    """
+    A class to create a multi-page Streamlit application.
+    """
 
+    def __init__(self, app_name):
+        self.pages = []
+        self.app_name = app_name
 
+        st.set_page_config(
+            page_title=self.app_name,
+            page_icon="🏦",
+        )
+
+    def add_page(self, title, func):
+        """
+        Add a new page to the application.
+        """
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        """
+        Run the application.
+        """
+        st.title(self.app_name)
+        page = st.sidebar.radio(
+            "Menu",
+            self.pages,
+            format_func=lambda page: page["title"])
+        page["function"]()
