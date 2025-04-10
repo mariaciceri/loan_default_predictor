ff6374d79c1cda90b34a058e828ff28d3b4bfdc9
Maria <mariafacchin00@hotmail.com>
2025-04-10 14:49:04 +0200

    Include set loading func

--- src/data_management.py
+++ src/data_management.py
@@ -7,6 +7,12 @@ def load_loan_data():
     df = pd.read_csv("outputs/datasets/collection/LoanDefault.csv")
     return df
 
+def load_sets():
+    X_train = pd.read_csv("outputs/ml_pipeline/predict_status/v1/X_train.csv")
+    y_train = pd.read_csv("outputs/ml_pipeline/predict_status/v1/y_train.csv")
+
+    return X_train, y_train
+
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
 
