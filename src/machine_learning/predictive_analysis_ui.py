00a79bc6f49ce1d1edea9e48deb28f10ed7b51a3
Maria <mariafacchin00@hotmail.com>
2025-04-10 10:25:39 +0200

    Add logic to predict on live data

---
+++ src/machine_learning/predictive_analysis_ui.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+
+
+def predict_status(X_live, status_pipeline_cleaning, status_pipeline_model):
+    # Preprocess the input data
+    X_live_cleaned = status_pipeline_cleaning.transform(X_live)
+
+    # Make predictions using the trained model
+    status_prediction = status_pipeline_model.predict(X_live_cleaned)
+    status_prediction_proba = status_pipeline_model.predict_proba(
+        X_live_cleaned
+        )
+
+    status_probability = status_prediction_proba[0][status_prediction]*100
+    if status_prediction == 1:
+        status_result = 'will'
+    else:
+        status_result = 'will not'
+
+
+    st.write(f"""There is a {status_probability[0].round(2)}% chance that the applicant
+{status_result} default.""")
+
+    return status_prediction[0]
