932507f219b2e2eb0dc4b959550422c026d7ea63
Maria <mariafacchin00@hotmail.com>
2025-04-10 10:25:16 +0200

    Include pipeline loading function

--- src/data_management.py
+++ src/data_management.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
+import joblib
 
 @st.cache_data
 def load_loan_data():
     df = pd.read_csv("outputs/datasets/collection/LoanDefault.csv")
     return df
+
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
 
